e4c96f28df9d7c22bac0953f26227d2caa006477
Kyle Black <kbshields@gmail.com>
2024-12-11 15:14:14 -0500

    Fixed Day 10 to use cleaner Zig approach

--- day_10/main.zig
+++ day_10/main.zig
@@ -33,17 +33,11 @@ fn part1(file: []const u8) !u64 {
     for (trailHeads.items) |head| {
         try searchList.append(head);
         defer searchList.clearRetainingCapacity();
-        // const nums = [_]u8{ '1', '2', '3', '4', '5', '6', '7', '8' };
-        //for (nums) |next| {
         for (1..9) |next| {
-
-            //            print("next type: {any}\n", .{@TypeOf(next)});
             var oldList = try searchList.clone();
             defer oldList.deinit();
             searchList.clearRetainingCapacity();
             for (oldList.items) |pathStep| {
-                if (pathStep[0] > 0)
-                    print("Grid: {any}\n", .{grid[pathStep[0] - 1][pathStep[1]]});
                 if (pathStep[0] > 0 and grid[pathStep[0] - 1][pathStep[1]] == next)
                     try searchList.append(pathStep - X);
                 if (pathStep[0] < grid_size and grid[pathStep[0] + 1][pathStep[1]] == next)
@@ -54,7 +48,6 @@ fn part1(file: []const u8) !u64 {
                     try searchList.append(pathStep + Y);
             }
         }
-        //print("searchList: {any}\n", .{searchList});
         var uniquePeaks = std.AutoHashMap(@Vector(2, u8), void).init(alloc);
         for (searchList.items) |pathStep| {
             if (pathStep[0] > 0 and grid[pathStep[0] - 1][pathStep[1]] == 9)
@@ -66,13 +59,9 @@ fn part1(file: []const u8) !u64 {
             if (pathStep[1] < grid_size and grid[pathStep[0]][pathStep[1] + 1] == 9)
                 uniquePeaks.put(pathStep + Y, {}) catch {};
         }
-        //print("Unique Peaks: {any}\n", .{uniquePeaks});
-        //print("Unique Peaks Count: {any}\n", .{@as(u64, @intCast(uniquePeaks.count()))});
         count += @intCast(uniquePeaks.count());
     }
 
-    //    print("Grid: {any}\n", .{grid});
-    //    print("trailHeads: {any}\n", .{trailHeads});
     return @intCast(count);
 }
 
@@ -90,7 +79,7 @@ fn part2(file: []const u8) !u64 {
     var x: usize = 0;
     while (lines.next()) |row| : (x += 1) {
         for (0.., row) |y, cell| {
-            grid[x][y] = cell;
+            grid[x][y] = try std.fmt.parseInt(u8, &[_]u8{cell}, 10);
             if (cell == '0') {
                 try trailHeads.append(.{ @intCast(x), @intCast(y) });
             }
@@ -103,9 +92,7 @@ fn part2(file: []const u8) !u64 {
     for (trailHeads.items) |head| {
         try searchList.append(head);
         defer searchList.clearRetainingCapacity();
-        const nums = [_]u8{ '1', '2', '3', '4', '5', '6', '7', '8' };
-        for (nums) |next| {
-            //            print("next type: {any}\n", .{@TypeOf(next)});
+        for (1..10) |next| {
             var oldList = try searchList.clone();
             defer oldList.deinit();
             searchList.clearRetainingCapacity();
@@ -120,25 +107,8 @@ fn part2(file: []const u8) !u64 {
                     try searchList.append(pathStep + Y);
             }
         }
-        //print("searchList: {any}\n", .{searchList});
-        var totalPeaks = std.ArrayList(@Vector(2, u8)).init(alloc);
-        for (searchList.items) |pathStep| {
-            if (pathStep[0] > 0 and grid[pathStep[0] - 1][pathStep[1]] == '9')
-                try totalPeaks.append(pathStep - X);
-            if (pathStep[0] < grid_size and grid[pathStep[0] + 1][pathStep[1]] == '9')
-                try totalPeaks.append(pathStep + X);
-            if (pathStep[1] > 0 and grid[pathStep[0]][pathStep[1] - 1] == '9')
-                try totalPeaks.append(pathStep - Y);
-            if (pathStep[1] < grid_size and grid[pathStep[0]][pathStep[1] + 1] == '9')
-                try totalPeaks.append(pathStep + Y);
-        }
-        //print("Unique Peaks: {any}\n", .{uniquePeaks});
-        //print("Unique Peaks Count: {any}\n", .{@as(u64, @intCast(uniquePeaks.count()))});
-        count += @intCast(totalPeaks.items.len);
+        count += @intCast(searchList.items.len);
     }
-
-    //    print("Grid: {any}\n", .{grid});
-    //    print("trailHeads: {any}\n", .{trailHeads});
     return @intCast(count);
 }
 
@@ -147,9 +117,4 @@ pub fn main() !void {
     print("Part 1: {d}\n", .{part1_ans});
     const part2_ans: u64 = try part2(input);
     print("Part 2: {d}\n", .{part2_ans});
-
-    //var it = std.mem.tokenizeAny(u8, input, "\n");
-    //while (it.next()) |line| {
-    //    print("{s}\n", .{line});
-    //}
 }
